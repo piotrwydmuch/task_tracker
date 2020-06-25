3b2563d61ef1c7d5b19e08931846a00c509c70e9
Piotr <piotrwydmuch95@gmail.com>
2020-06-25 11:14:24 +0200

    Components are rdy

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EditExercise extends Component {
+  render() {
+    return (
+      <div>
+        <p>It's Edit Exercise component.</p>
+      </div>
+    );
+  }
+}
+
+export default EditExercise;
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ExerciseList extends Component {
+  render() {
+    return (
+      <div>
+        <p>It's Exercises list component.</p>
+      </div>
+    );
+  }
+}
+
+export default ExerciseList;
